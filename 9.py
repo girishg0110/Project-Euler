c6b2312e790cf5a919b35303bc76d2a2c3a0a2ac
girishg0110 <girishg0110@gmail.com>
2023-11-28 22:34:38 -0500

    solved #9

---
+++ 9.py
@@ -0,0 +1,21 @@
+# Description: 
+
+import math
+import numpy as np
+
+def solve(n):
+    s = 0
+    for a in range(1, n+1):
+        asq = a**2
+        for b in range(a+1, n+1):
+            bsq = b**2
+            c = n - a - b
+            if (b < c) and (asq + bsq == c**2):
+                return a*b*c
+
+    return s
+
+if __name__ == "__main__":
+    n = 1000
+    solution = solve(n)
+    print(solution)
