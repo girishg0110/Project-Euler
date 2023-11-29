c7c217f9d2619a81032065ae402fefbec24162a2
girishg0110 <girishg0110@gmail.com>
2023-11-28 23:07:40 -0500

    added python templates

---
+++ templates/template.py
@@ -0,0 +1,13 @@
+# Description: 
+
+import math
+import numpy as np
+
+def solve(n):
+    s = 0
+    return s
+
+if __name__ == "__main__":
+    n = 100
+    solution = solve(n)
+    print(solution)
